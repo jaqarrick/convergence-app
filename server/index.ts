a73548aeefee68b36307f90af5137043ff597bec
jaqarrick <pigmaask@gmail.com>
2020-11-04 14:20:58 -0800

    staged app for production

--- server/index.ts
+++ server/index.ts
@@ -1,6 +1,24 @@
 import express from "express"
 const app = express()
 import http from "http"
+import compression from "compression"
+import morgan from "morgan"
+import path, { normalize } from "path"
+const normalizePort = port => parseInt(port, 10)
+const PORT = normalizePort(process.env.PORT || 5000)
+const dev = app.get("env") !== "production"
+
+if (!dev) {
+	app.disable("x-powered-by")
+	app.use(compression())
+	app.use(morgan("common"))
+
+	app.use(express.static(path.resolve(__dirname, "..", "build")))
+	app.get("*", (req, res) => {
+		res.sendFile(path.resolve(__dirname, "build", "index.html"))
+	})
+}
+
 const server = new http.Server(app)
 
 import ioSocket from "socket.io"
@@ -90,7 +108,6 @@ io.on("connection", (socket: any) => {
 	})
 })
 
-const PORT = 5000
 server.listen(PORT, () => {
 	console.log("backend server is up and running on Port: " + PORT)
 })
