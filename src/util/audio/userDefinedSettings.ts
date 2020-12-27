2e857681b463f1bad180f03072aa6afbb98d71fc
jaqarrick <pigmaask@gmail.com>
2020-12-26 16:42:20 -0800

    removed unused user settings

--- src/util/audio/userDefinedSettings.ts
+++ src/util/audio/userDefinedSettings.ts
@@ -16,26 +16,26 @@ enum settingsName {
 }
 
 const userDefinedSettings: userSettingsObject[] = [
-	{
-		settingsGroup: settingsGroup.inputs,
-		id: uuid(),
-		options: [
-			{
-				name: settingsName.input,
-				id: uuid(),
-				params: [
-					{
-						paramName: "connected?",
-						minVal: 0,
-						maxVal: 1,
-						value: 0,
-						id: uuid(),
-						rangeAmount: 'thousand'
-					},
-				],
-			},
-		],
-	},
+	// {
+	// 	settingsGroup: settingsGroup.inputs,
+	// 	id: uuid(),
+	// 	options: [
+	// 		{
+	// 			name: settingsName.input,
+	// 			id: uuid(),
+	// 			params: [
+	// 				{
+	// 					paramName: "connected?",
+	// 					minVal: 0,
+	// 					maxVal: 1,
+	// 					value: 0,
+	// 					id: uuid(),
+	// 					rangeAmount: 'thousand'
+	// 				},
+	// 			],
+	// 		},
+	// 	],
+	// },
 ]
 
 export default userDefinedSettings
